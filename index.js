280ae66e74a03fc1b6dcb3a196bf6a7299cb64d6
CodeLHC <lhcrawford97@gmail.com>
2023-10-02 10:56:44 +0100

    updated function to populate board with array

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const gameBoard = (() => {
   const board = new Array(9);
   const testBoard = ["O", "X", "X", "O", "X", "O", "X", "X", "O"];
-  const populateDisplay = () => {
-    const display = document.getElementById("display");
-    testBoard.forEach((element) => {
-      display.innerText = display.innerText + element + "|";
-    });
+  const populateTable = () => {
+    for (let i = 0; i < testBoard.length; i++) {
+      document.getElementById(i).innerText = testBoard[i];
+      console.log(i, testBoard[i]);
+    }
   };
-  return { populateDisplay };
+  return { populateTable };
 })();
 
 const player = () => {};
 
 const game = () => {};
 
-gameBoard.populateDisplay();
+gameBoard.populateTable();
