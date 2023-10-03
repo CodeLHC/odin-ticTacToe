cad3e513873f66c1c433417f3862a1e631adebf3
CodeLHC <lhcrawford97@gmail.com>
2023-10-03 11:17:15 +0100

    logic for winning

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const gameBoard = (() => {
   const board = new Array(9);
-  const testBoard = ["O", "X", "X", "O", "X", "O", "X", "X", "O"];
+  const m = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  //   const testBoard = ["O", "X", "X", "O", "X", "O", "X", "X", "O"];
   const populateTable = () => {
     for (let i = 0; i < board.length; i++) {
-      document.getElementById(i).innerText = board[i];
+      //   console.log(board[i]);
+      //   if (board[i] === undefined) {
+      //     document.getElementById(i).innerText = " ";
+      //   }
+      //   document.getElementById(i).innerText = board[i];
+      document.getElementById(i).innerText = "";
     }
   };
-  return { board, populateTable };
+
+  const gameOutcome = () => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winningCombinations.forEach((element) => {
+      if (
+        element.every((number) => {
+          return board[number] === "X";
+        })
+      ) {
+        console.log("player 2 wins");
+      } else if (
+        element.every((number) => {
+          return board[number] === "O";
+        })
+      ) {
+        console.log("player 1 wins");
+      } else return;
+    });
+  };
+  return { board, populateTable, gameOutcome };
 })();
 
 const player = (name, mark) => {
@@ -15,21 +54,32 @@ const player = (name, mark) => {
   const placeMarker = () => {
     document.querySelectorAll(".box").forEach((box) => {
       box.addEventListener("click", (e) => {
+        if (box.innerText) return;
+        if (mark === "X") {
+          mark = "O";
+        } else if (mark === "O") {
+          mark = "X";
+        }
         box.innerText = mark;
         const index = e.target.id;
         gameBoard.board[index] = mark;
+
+        gameBoard.gameOutcome();
       });
     });
   };
+
   return { getName, getMarker, placeMarker };
 };
 
 const playerOne = player(1, "O");
-console.log(playerOne.getMarker());
+const playerTwo = player(2, "X");
 
 const runGame = () => {
   gameBoard.populateTable();
+
   playerOne.placeMarker();
+  playerTwo.placeMarker();
 };
 
 runGame();
